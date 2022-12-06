36b6a818574db129c9fd0203129ac5f5fa187d93
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-06 15:49:57 +0900

    회원가입 관련 DTO

---
+++ src/main/java/com/sparta/myboard/dto/SignupRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.myboard.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SignupRequestDto {
+
+    private String username;
+    private String password;
+
+}
